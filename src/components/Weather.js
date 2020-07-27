d383df1f8655af739ab7cdef466fe3ad86b29475
Roninanu777 <ronirajkamalpradhan1112@gmail.com>
2020-07-27 06:25:55 +0530

    Cleaning up

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -2,13 +2,10 @@ import React from 'react';
 import styled, {keyframes} from 'styled-components';
 
 //-------------------------------------------------------------------------------------------------------
-const Up = keyframes`
-    top: -1rem;
-`;
-
 const Container = styled.div`
     background-color: white;
     display: flex;
+    position: relative;
     flex-direction: column;
     align-items: center;
     justify-content: center;
@@ -17,10 +14,10 @@ const Container = styled.div`
     padding: 2rem 1.5rem;
     transition: all 0.2s ease;
     border-radius: 5px;
-    box-shadow: 0 .1rem 1rem 5px rgba(0,0,0,0.01);
+    box-shadow: 0 .1rem 1rem 5px rgba(0,0,0,0.07);
     &:hover{
-        box-shadow: 0 .1rem 1rem 5px rgba(0,0,0,0.1);
-        animation: ${Up} 0.2s ease-out;
+        box-shadow: 0 .1rem 1rem 5px rgba(0,0,0,0.16);
+        transform: translateY(-3px);
     }
 `;
 
