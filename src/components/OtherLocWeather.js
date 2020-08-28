2329e09d47316a43d5b3632b5d7bd8afd461d508
Roninanu777 <ronirajkamalpradhan1112@gmail.com>
2020-08-28 21:36:47 +0530

    api call done

--- src/components/OtherLocWeather.js
+++ src/components/OtherLocWeather.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
-import { faMapMarkedAlt, faPlus } from "@fortawesome/free-solid-svg-icons";
+import { faMapMarkedAlt } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Modal from './Modal';
 
@@ -26,44 +26,23 @@ const P = styled.p`
     font-family: 'Varela Round', sans-serif;
 `;
 
-const Add = styled.div`
-    display: flex;
-    width: 100%;
-    align-items: center;
-    margin-top: 3rem;
-    justify-content: center;
-`;
-
 const AddError = styled(P)`
     color: red;
     font-size: 1rem;
 `;
 
-const Button = styled.button`
-    border: none;
-    padding: 7px 10px;
-    border-radius: 50%;
-    background-color: #2b7a78;
-    outline: none;
-    cursor: pointer;
-`;
-
 //----------------------------------------------------------------------------------//
 
 const OtherLocWeather = () => {
-    let [clicked, setClicked] = useState(false);
 
-    
     return (
         <Container>
             <Other>
                 <FontAwesomeIcon style={{marginRight: '.6rem'}} icon={faMapMarkedAlt}></FontAwesomeIcon>
                 <P>Other Locations</P>
             </Other>
-            { clicked ? <Modal /> : <AddError>Click below to add locations</AddError>}
-            <Add>
-                <Button onClick={() => setClicked(!clicked)}><FontAwesomeIcon style={{fontSize: '1.7rem', color: 'white'}} icon={faPlus}></FontAwesomeIcon></Button>
-            </Add>
+            <AddError>Click below to add locations</AddError>
+            
         </Container>
     )
 }
