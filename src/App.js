e8ce7c955e8067eba2807b536e1cacfe2b3d82ce
Roninanu777 <ronirajkamalpradhan1112@gmail.com>
2020-08-28 21:37:10 +0530

    added overlay effect

--- src/App.js
+++ src/App.js
@@ -1,17 +1,63 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import CurrLocWeather from './components/CurrLocWeather';
 import OtherLocWeather from './components/OtherLocWeather';
+import Modal from './components/Modal';
+
+const Overlay = styled.div`
+    position: absolute;
+    background-color: rgba(85, 99, 99, 0.7);
+    height: 100%;
+    transition: all 1.5s ease-in-out;
+    width: 100%;
+    align-self: center;
+`;
+
+const Add = styled.div`
+    display: flex;
+    width: 100%;
+    align-items: center;
+    margin-top: 3rem;
+    justify-content: center;
+`;
+
+const Button = styled.button`
+    border: none;
+    padding: 7px 10px;
+    border-radius: 50%;
+    background-color: #2b7a78;
+    outline: none;
+    cursor: pointer;
+`;
 
 class App extends Component {
+    state={
+      open: false
+    }
 
-  render() {
-    return (
-      <React.Fragment>
-        <CurrLocWeather />
-        <OtherLocWeather />
-      </React.Fragment>
-    )
-  }
+    handleOpen = () => {
+      this.setState({open: true})
+    }
+
+    handleClose = () => {
+      this.setState({open: false})
+    }
+
+    render() {
+      return (
+        <React.Fragment>
+          <CurrLocWeather />
+          <OtherLocWeather />
+          <Add>
+              <Button onClick={this.handleOpen}><FontAwesomeIcon style={{fontSize: '1.7rem', color: 'white'}} icon={faPlus}></FontAwesomeIcon></Button>
+          </Add>
+          { this.state.open ? <Overlay onClick={this.handleClose} /> : null}
+          <Modal show={this.state.open} close={this.handleClose} />
+        </React.Fragment>
+      )
+    }
 }
 
 export default App;
