e4b6d26ee66961a24776cea9795b9ca6263f6011
Roninanu777 <ronirajkamalpradhan1112@gmail.com>
2020-09-10 23:45:03 +0530

    saved location component

---
+++ src/components/SavedLoc.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import styled from 'styled-components';
+import SearchedCity from './SearchedCity';
+
+const Container = styled.div`
+    background-color: white;
+    display: flex;
+    position: relative;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+    cursor: pointer;
+    padding: 2rem 0;
+    margin-top: 2rem;
+    transition: all 0.2s ease;
+    border-radius: 5px;
+    box-shadow: 0 .1rem 1rem 5px rgba(0,0,0,0.07);
+    &:hover{
+        box-shadow: 0 .1rem 1rem 5px rgba(0,0,0,0.16);
+        transform: translateY(-3px);
+    }
+`;
+
+export default function SavedLoc() {
+    return (
+        <>
+            <Container>
+                Saved
+            </Container>
+            <Container>
+                Saved
+            </Container>
+            <Container>
+                Saved
+            </Container>
+            <Container>
+                Saved
+            </Container>
+            <Container>
+                Saved
+            </Container>
+        </>
+    )
+}
