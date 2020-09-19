b1abd7b93e936a7e8b88ad0cfb150eca9ada95a7
Roni Raj Kamal Pradhan <ronirajkamalpradhan1112@gmail.com>
2020-09-19 11:45:36 +0530

    cleaning up

--- src/components/SearchedCity.js
+++ src/components/SearchedCity.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from  'react';
+import React, {useState} from  'react';
 import styled from 'styled-components';
 
 const Container = styled.div`
@@ -26,6 +26,7 @@ const SearchedCity = (props) => {
     let handleId = () => {
         let ids = JSON.parse(localStorage.getItem('ids')) || [];
         setId([...ids, props.cityId]);
+        props.reset();
         props.close();
     }
 
